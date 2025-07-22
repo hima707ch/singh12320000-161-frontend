c6844f0fb2f26a0e0941ccdeee282bd72852bd6d
root <root@09375da5186a>
2025-07-22 13:12:43 +0000

    Frontend setup and components update

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,21 +1,19 @@
-import React, { useEffect } from 'react';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./Global/Header.jsx"
+import HomePage from "./HomePage/Body.jsx";
+import PropertyListingPage from "./PropertyListingPage/Body.jsx";
 
 const Body = () => {
-  useEffect(() => {
-    const interval = setInterval(() => {
-      window.location.reload();
-    }, 4000); // refresh every 4 seconds
-
-    return () => clearInterval(interval); // cleanup on unmount
-  }, []);
-
-  return (
-    <div className="flex items-center justify-center h-screen bg-gray-100">
-      <div className="text-xl font-semibold text-gray-700 bg-white px-6 py-4 rounded shadow">
-        Loading Preview...
-      </div>
-    </div>
-  );
+    return (
+        <Router>
+        <Header />
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/propertylistingpage" element={<PropertyListingPage />} />
+            </Routes>
+        </Router>
+    );
 };
 
 export default Body;
